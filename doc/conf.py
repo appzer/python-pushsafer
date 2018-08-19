82ea308e23ba4819c0011b3bf33c925940eb82c9
Kevin Siml <appzer@users.noreply.github.com>
2018-08-19 16:25:50 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'python-pushsafer'
-copyright = u'2017'
+copyright = u'2018'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

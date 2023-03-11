2983b9f2618886db1f2d8dca0dae1db65e6bc57c
Kevin Siml <appzer@users.noreply.github.com>
2023-03-11 12:10:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="python-pushsafer",
-    version="1.0",
+    version="1.1",
     description="Comprehensive bindings for the " "Pushsafer.com notification service",
     long_description=open("README.rst").read()
     + "\n"
